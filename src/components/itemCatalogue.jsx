286c73c316c9f17d3f29779310210fbec2d03478
Ignacio Veiga <ignacioveigagiusti@gmail.com>
2022-01-22 23:11:25 -0300

    Desafío: Contador con botón V1

---
+++ src/components/itemCatalogue.jsx
@@ -0,0 +1,10 @@
+export const products = [
+    { id:'1', category:'product', name:'Lana de Vidrio (m)', price:450 , picture:''},
+    { id:'2', category:'product', name:'Placa de Yeso (m2)', price:500 , picture:''},
+    { id:'3', category:'product', name:'Placa de Lana de Vidrio', price:450 , picture:''},
+    { id:'4', category:'product', name:'Vidrio PVB (m2)', price:10000 , picture:''},
+    { id:'5', category:'service', name:'Asesoramiento (h)', price:1500 , picture:''},
+    { id:'6', category:'service', name:'Instalación en Cerramientos Existentes (h)', price:1500 , picture:''},
+    { id:'7', category:'service', name:'Planificación de Cerramientos a Construir (h)', price:1500 , picture:''},
+    { id:'8', category:'service', name:'Mantenimiento (h)', price:1000 , picture:''},
+]
