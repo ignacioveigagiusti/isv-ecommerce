412033ec3dc9cbc96a21bd084383e6acf389a2f6
Ignacio Veiga <ignacioveigagiusti@gmail.com>
2022-02-08 22:53:53 -0300

    public directory for images

--- src/components/cartWidget/cartWidget.jsx
+++ src/components/cartWidget/cartWidget.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import cartIcon from '../../assets/cartIcon.svg';
 import styles from './cartWidget.module.css'
 
 export default function CartWidget({quantitySum}) {
     
     return (  
         <button className={`btn btn-outline-secondary ${styles.cartWidget}`} href='#'>
-            <img src={cartIcon} className="cartIcon" alt='Cart'/>
+            <img src='/assets/cartIcon.svg' className="cartIcon" alt='Cart'/>
             <p className={(quantitySum > 0 ? `${styles.cartQuantity}` : '')}>{quantitySum}</p>
         </button>
     );
